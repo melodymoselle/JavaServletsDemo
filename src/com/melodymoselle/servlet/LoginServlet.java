f610cb40f28c8b2e3931a62534655b33369b1aa8
Melmo <melodymoselle@gmail.com>
2017-03-25 15:42:15 -0700

    Manage user session with cookies

--- src/com/melodymoselle/servlet/LoginServlet.java
+++ src/com/melodymoselle/servlet/LoginServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,66 +15,34 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet(
-		description = "Login Servlet", 
-		urlPatterns = { "/LoginServlet" }, 
-		initParams = { 
-				@WebInitParam(name = "user", value = "Pankaj"), 
-				@WebInitParam(name = "password", value = "journaldev")
-		})
+@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-	public void init() throws ServletException {
-		//we can create DB connection resource here and set it to Servlet context
-		if(getServletContext().getInitParameter("dbURL").equals("jdbc:mysql://localhost/mysql_db") &&
-				getServletContext().getInitParameter("dbUser").equals("mysql_user") &&
-				getServletContext().getInitParameter("dbUserPwd").equals("mysql_pwd"))
-		getServletContext().setAttribute("DB_Success", "True");
-		else throw new ServletException("DB Connection error");
-	}
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	private final String userID = "Pankaj";
+	private final String password = "journaldev";
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	 */protected void doPost(HttpServletRequest request,
+				HttpServletResponse response) throws ServletException, IOException {
 
-		//get request parameters for userID and password
-		String user = request.getParameter("user");
-		String pwd = request.getParameter("pwd");
-		
-		//get servlet config init params
-		String userID = getServletConfig().getInitParameter("user");
-		String password = getServletConfig().getInitParameter("password");
-		//logging example
-		log("User="+user+"::password="+pwd);
-		
-		if(userID.equals(user) && password.equals(pwd)){
-			response.sendRedirect("LoginSuccess.jsp");
-		}else{
-			RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
-			PrintWriter out= response.getWriter();
-			out.println("<font color=red>Either user name or password is wrong.</font>");
-			rd.include(request, response);
+			// get request parameters for userID and password
+			String user = request.getParameter("user");
+			String pwd = request.getParameter("pwd");
 			
+			if(userID.equals(user) && password.equals(pwd)){
+				Cookie loginCookie = new Cookie("user",user);
+				//setting cookie to expiry in 30 mins
+				loginCookie.setMaxAge(30*60);
+				response.addCookie(loginCookie);
+				response.sendRedirect("LoginSuccess.jsp");
+			}else{
+				RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+				PrintWriter out= response.getWriter();
+				out.println("<font color=red>Either user name or password is wrong.</font>");
+				rd.include(request, response);
+			}
+
 		}
-		
-	}
 
 }

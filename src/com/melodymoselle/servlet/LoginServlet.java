d38cffb100d91a200fc2396d02707290e1794486
Melmo <melodymoselle@gmail.com>
2017-03-25 16:32:29 -0700

    Manage session with HttpSession

--- src/com/melodymoselle/servlet/LoginServlet.java
+++ src/com/melodymoselle/servlet/LoginServlet.java
@@ -7,47 +7,20 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet(
-		description = "Login Servlet", 
-		urlPatterns = { "/LoginServlet" }, 
-		initParams = { 
-				@WebInitParam(name = "user", value = "Pankaj"), 
-				@WebInitParam(name = "password", value = "journaldev")
-		})
+@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-	public void init() throws ServletException {
-		//we can create DB connection resource here and set it to Servlet context
-		if(getServletContext().getInitParameter("dbURL").equals("jdbc:mysql://localhost/mysql_db") &&
-				getServletContext().getInitParameter("dbUser").equals("mysql_user") &&
-				getServletContext().getInitParameter("dbUserPwd").equals("mysql_pwd"))
-		getServletContext().setAttribute("DB_Success", "True");
-		else throw new ServletException("DB Connection error");
-	}
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	private final String userID = "admin";
+	private final String password = "password";
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -58,13 +31,14 @@ public class LoginServlet extends HttpServlet {
 		String user = request.getParameter("user");
 		String pwd = request.getParameter("pwd");
 		
-		//get servlet config init params
-		String userID = getServletConfig().getInitParameter("user");
-		String password = getServletConfig().getInitParameter("password");
-		//logging example
-		log("User="+user+"::password="+pwd);
-		
 		if(userID.equals(user) && password.equals(pwd)){
+			HttpSession session = request.getSession();
+			session.setAttribute("user", "melmo");
+			//setting session to expire in 30 min
+			session.setMaxInactiveInterval(30*60);
+			Cookie userName = new Cookie("user", user);
+			userName.setMaxAge(30*60);
+			response.addCookie(userName);
 			response.sendRedirect("LoginSuccess.jsp");
 		}else{
 			RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
